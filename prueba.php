0905d7ae75dbd50b42ca70eb28a4c61a92d97851
Adrián Laya García <superlaya50@gmail.com>
2022-07-20 17:15:48 +0200

    // Inclusión de un archivo

---
+++ prueba.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola Adri!</h1>
+</body>
+</html>
+
+<style>
+    h1{
+        font: 1em sans-serif;
+        width: 100%;
+        margin-top:1em;
+        background-color: red;
+        text-align: center; 
+    }
+</style>
+
+<!--https://www.oscarlijo.com/blog/mejores-extensiones-para-visual-studio-code/  para pillar el header-->
